3f80cdf75e993df9b363de7332795fdbaf1e9556
Sam Samskies <sam@strike.me>
2022-12-19 16:03:08 -0600

    add basic UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider, Box } from "@chakra-ui/react";
+import { Montserrat } from "@next/font/google";
+import theme from "styles/theme";
+
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Box className={montserrat.className}>
+        <Component {...pageProps} />
+      </Box>
+    </ChakraProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 import Head from "next/head";
-import { Heading, Text } from "@chakra-ui/react";
+import {
+  Heading,
+  Text,
+  Container,
+  Input,
+  Switch,
+  VStack,
+  HStack,
+} from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -11,8 +19,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Heading as="h1">Santa Sats Bot</Heading>
-        <Text>Bitcoin Lightning DCA bot POC</Text>
+        <Container>
+          <Heading as="h1" mt={10} mb={4}>
+            Santa Sats Bot
+          </Heading>
+          <VStack mb={4} spacing={4} align="flex-start">
+            <Text>
+              This is a POC for a bot that uses the Strike API to send sats to a
+              specified Lightning Address every min. Your API key must have the
+              partner.payment-quote.lightning.create scope in order for this app
+              to work. This currently only works with testnet Lightning
+              Addresses.
+            </Text>
+            <Text>Enter your Lightning Address and toggle switch on to start stacking.</Text>
+          </VStack>
+          <HStack>
+            <Input
+              placeholder="Lightning Address e.g. sam_test2@next.strike.army"
+              variant="filled"
+              size="lg"
+              autoFocus
+            />
+            <Switch size="lg" />
+          </HStack>
+        </Container>
       </main>
     </>
   );

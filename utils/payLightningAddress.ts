3e455cbdf4b4e624aec3e1cd25fdfe4611f260c8
Sam Samskies <samsamskies@gmail.com>
2022-12-29 16:54:09 -1000

    upgrade lnurl-pay and simplify code

--- utils/payLightningAddress.ts
+++ utils/payLightningAddress.ts
@@ -1,8 +1,13 @@
-import { getInvoice } from "utils/lnurlPay";
 import { createPaymentQuote, executePaymentQuote } from "utils/strikeApi";
+import { requestInvoice } from "lnurl-pay";
+import { Satoshis } from "lnurl-pay/dist/types/types";
 
-export const payLightningAddress = async (lightningAddress: string) => {
-  const invoice = await getInvoice(lightningAddress);
+export const payLightningAddress = async (lightningAddress: string, tokens: number = 1000) => {
+  const { invoice } = await requestInvoice({
+    lnUrlOrAddress: lightningAddress,
+    tokens: tokens as Satoshis,
+    validateInvoice: true,
+  });
   const { paymentQuoteId } = await createPaymentQuote(invoice);
 
   return executePaymentQuote(paymentQuoteId);
